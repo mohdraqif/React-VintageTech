5410d597e3bfbd2ed328ef59aba66d8e077b34c7
mohdraqif <mohdraqif94@gmail.com>
2020-03-08 15:42:55 +0530

    Inot commit

---
+++ src/pages/Products.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { ProductContext } from "../context/products";
+import Loading from "../components/Loading";
+import PageProducts from "../components/Products/PageProducts";
+import Filters from "../components/Products/Filters";
+export default function Products() {
+  const { loading } = React.useContext(ProductContext);
+
+  if (loading) {
+    return <Loading />;
+  }
+  return (
+    <>
+      <Filters />
+      <PageProducts></PageProducts>
+    </>
+  );
+}
